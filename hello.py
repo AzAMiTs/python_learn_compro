3cdd67d4ec8ef7a5875b6391263cd78bb868ff78
AzAMiTs <thanapatrattnawan@gmail.com>
2026-07-01 11:08:26 +0700

    all

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+print("Hello, World!");
+print('Information Network Engineering');
+print('Thanaphat Rattnawan you will be a good programmer');
+
+print('I\'m Thanaphat, I will be a good programmer');
+print('Be ask me "Who got the book?"');
+print("I'm good and \"I will do my best\"");
+print("/\\");
---
+++ testlist.py
@@ -0,0 +1,5 @@
+from math import pi
+print(pi)
+print('%.2f' %pi)
+print('%.50f' %pi)
+print('%.60f' %pi)
